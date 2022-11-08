df148cb88301bca10acae43f4f55e1564f2954da
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-11-08 10:58:50 -0700

    data and loose first test

---
+++ __tests__/books.test.js
@@ -0,0 +1,20 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('book routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('should return list of books', () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(9);
+    const hobbit = res.body.find((book) => book.id === '1');
+    expect(hobbit).toHaveProperty('title', 'The Hobbit');
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,48 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS books;
+
+
+CREATE TABLE authors (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  dob DATE,
+  pob VARCHAR
+);
+
+CREATE TABLE books (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR NOT NULL,
+  released INT NOT NULL
+);
+
+INSERT INTO authors (
+    name,
+    dob,
+    pob
+)
+VALUES
+  ('J.R.R. Tolkien', '1892-01-03', 'Bloemfontein, South Africa'),
+  ('Patrick Rothfus', '1973-06-06', 'Madison, WI'),
+  ('Neil Gaiman', '1960-11-10', 'Portchester, UK'),
+  ('Mary Shelley', '1797-08-30', 'Somers Town, UK'),
+  ('Brandon Sanderson', '1975-12-19', 'Lincoln, NE'),
+  ('Christopher Tolkien', '1924-11-21', 'Leeds, UK')
+;
+
+INSERT INTO books (
+    title,
+    released
+)
+VALUES
+  ('The Hobbit', 1937),
+  ('The Way of Kings', 2010),
+  ('Skyward', 2018),
+  ('Frankenstien', 1818),
+  ('The Mortal Immortal', 1833),
+  ('American Gods', 2001),
+  ('The Name of the Wind', 2007),
+  ('The Wise Mans Fear', 2011),
+  ('The Silmarillion', 1977)
+;
